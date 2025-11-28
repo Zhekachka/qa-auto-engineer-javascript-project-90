d41b69ab2839be5eb4e3a79f56ae1e160af2bce7
EvgeniyaKokareva <zhekachkakokareva@yandex.ru>
2025-11-28 19:50:13 +0300

    Шаг №3 Аутентификация и авторизация

---
+++ e2e/auth.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { userData, AuthPage } from './utils/utils.js';
+
+test.describe('Аутентификация и авторизация', () => {
+  let authPage;
+
+  test.beforeEach(async ({ page }) => {
+    authPage = new AuthPage(page);
+    await authPage.goto();
+  });
+
+  test('Проверка открытия приложения и логин', async ({ page }) => {
+
+    await authPage.login(userData.username, userData.password);
+    await expect(await authPage.getWelcomeHeader()).toBeVisible();
+  })
+
+  test('Разлогин', async ({ page }) => {
+
+    await authPage.login(userData.username, userData.password);
+    await authPage.logout();
+    await expect(await authPage.getSignInButton()).toBeVisible();
+  })
+})
