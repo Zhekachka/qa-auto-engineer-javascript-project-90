fa776a7a7bf777b051444980199c54698ea3ee5f
EvgeniyaKokareva <zhekachkakokareva@yandex.ru>
2025-12-12 07:23:18 +0300

    Перенос в папку Tests

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ import process from 'node:process'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
